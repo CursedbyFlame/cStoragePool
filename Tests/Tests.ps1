06da9209e8336c2c8a179bf72d95ca0dc039740d
Dzmitry Nikalaichyk <dzmitry_nikalaichyk@epam.com>
2016-03-14 20:26:43 +0300

    Modified test income.

---
+++ Tests/Tests.ps1
@@ -0,0 +1,23 @@
+﻿$DSCModuleName = 'cStoragePool'
+$DSCResourceName = 'cStoragePool'
+
+$Splat = @{
+    Path = $PSScriptRoot
+    ChildPath = "..\..\DSCResources\$DSCResourceName\$DSCResourceName.psm1"
+    Resolve = $true
+    ErrorAction = 'Stop'
+}
+$DSCResourceModuleFile = Get-Item -Path (Join-Path @Splat)
+
+if (Get-Module -Name $DSCResourceName)
+{
+    Remove-Module -Name $DSCResourceName
+}
+
+Import-Module -Name $DSCResourceModuleFile.FullName -Force
+if (!(Get-Module -Name Pester))
+{
+    Import-Module -Name "$env:System_DefaultWorkingDirectory\Pester\3.3.5\pester.psm1" -Force
+}
+
+Invoke-Pester -Script "$env:System_DefaultWorkingDirectory\tests\unit\cStoragePool.psm1" -EnableExit
