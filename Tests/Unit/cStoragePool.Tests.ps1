dd4cb563379ed2672970a62abcca11700da7a5b7
Dzmitry Nikalaichyk <dzmitry_nikalaichyk@epam.com>
2016-03-14 17:14:53 +0300

    Added elevated functionality to tests.

--- Tests/Unit/cStoragePool.Tests.ps1
+++ Tests/Unit/cStoragePool.Tests.ps1
@@ -1,4 +1,23 @@
-﻿$DSCModuleName = 'cStoragePool'
+﻿$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+if ($myWindowsPrincipal.IsInRole($adminRole))
+{
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+}
+else
+{
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+    $newProcess.Verb = "runas";
+    [System.Diagnostics.Process]::Start($newProcess);
+    exit
+}
+
+$DSCModuleName = 'cStoragePool'
 $DSCResourceName = 'cStoragePool'
 
 $Splat = @{
